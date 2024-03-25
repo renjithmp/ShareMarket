ff91621634e621a7d1e51209a2fa95b4bc0e71b5
renjithmp <renjithmac@gmail.com>
2024-03-25 09:58:30 +0530

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ShareMarket;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ShareMarket;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
